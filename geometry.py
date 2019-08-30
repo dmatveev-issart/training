5390a4a945cc896a9f87c1ec485b533ad7e3732f
dmatveev <dmatveev@issart.com>
2019-08-30 15:24:09 +0600

    The files added

---
+++ geometry.py
@@ -0,0 +1,13 @@
+from geom2d.point import *
+
+a = Point(0, 0)
+b = Point(3, 4)
+
+#print(a.distance(b))
+#print(a == b)
+#print(a == Point(0, 0))
+
+l1 = [Point(0, 0), Point(1, 2), Point(2, 1)]
+l2 = sorted(l1)
+print(l1)
+print(l2)
