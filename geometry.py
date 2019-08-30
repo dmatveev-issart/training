7291a0a912be7a0352565cf0ee1c5af870189947
dmatveev <dmatveev@issart.com>
2019-08-30 16:43:21 +0600

    lambda function added

--- geometry.py
+++ geometry.py
@@ -7,7 +7,17 @@ b = Point(3, 4)
 #print(a == b)
 #print(a == Point(0, 0))
 
-l1 = [Point(0, 0), Point(1, 2), Point(2, 1)]
-l2 = sorted(l1)
+l1 = [Point(0, 0), Point(1, 2), Point(2, 1), Point(3, 5)]
+
+
+#def x(p):
+#    return p.x
+
+
+l2 = sorted(l1, key=lambda p: p.x)
+l3 = sorted(l1, key=lambda p: p.distance(Point(0, 0)))
+
 print(l1)
 print(l2)
+print(l3)
+
